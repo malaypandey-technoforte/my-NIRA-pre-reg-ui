73433128229a49892c083fc8e34cc48ff2e04fd6
TugumeDavis <davis.tugume@nira.go.ug>
2024-03-29 11:18:35 +0300

    Contact Us Page

--- pre-registration-ui/src/app/core/contact/contact.component.ts
+++ pre-registration-ui/src/app/core/contact/contact.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+import { AppComponent } from '../../app.component';
+
+@NgModule({
+  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class ContactComponent implements OnInit{
-  form: FormGroup;
-
-  ngOnInit(): void {
-    this.buildForm();
-  }
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  send(): void {
-    const { name, email, message } = this.form.value;
-    alert(`Name: ${name}, Email: ${email}, Message: ${message} `);
-  }
-
-  private buildForm(): void {
-    this.form = this.formBuilder.group({
-      name: this.formBuilder.control(null),
-      email: this.formBuilder.control(null),
-      message: this.formBuilder.control(null),
-    });
-  }
-}
+export class AppModule {}
